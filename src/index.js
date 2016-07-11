75f38e6e2782bef27fe5af682eec516755f1feee
kelsonic <kgadams93@gmail.com>
2016-07-10 21:28:08 -0700

    Update gitignore and main index component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
+
+// Declare env vars
 
 
+class App extends Component {
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({
+      key: API_KEY,
+      term: 'surfboards'
+    }, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
 };
 
 // Take the component and put it into the page
