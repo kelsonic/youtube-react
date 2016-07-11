44c65fc46ba6b7a2b12078e13ef8d51b2a564df5
kelsonic <kgadams93@gmail.com>
2016-07-10 18:43:28 -0700

    Create app component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// Create new component.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+// Take the component and put it into the page
+ReactDOM.render(<App />, document.querySelector('#app'));
