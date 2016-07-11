e01c4ee1ffdf8a3519d5ad1987b266e8d782a020
kelsonic <kgadams93@gmail.com>
2016-07-10 21:26:37 -0700

    Add onclick listener with onvideoselect prop method to switch current video detail

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
+
         <div className="media-left">
           <img className="media-object" src={imageUrl} />
         </div>
@@ -15,6 +16,7 @@ const VideoListItem = ({video}) => {
             {video.snippet.title}
           </div>
         </div>
+
       </div>
     </li>
   );
